1b954db911ac760ce191771f3d4846f917b08843
Rick Dawson <dawsora@gmail.com>
2023-01-17 23:35:23 -0500

    fixed missing #

--- index.js
+++ index.js
@@ -13,39 +13,39 @@ function guessNum() {
     counter++;
 
     // update the guess counter
-    document.querySelector('counter').innerHTML = counter;
+    document.querySelector('#counter').innerHTML = counter;
 
     //Clear guess input
     let guessNumber = 0;
 
     //get the current guess number
-    guessNumber = document.querySelector('guess').value;
+    guessNumber = document.querySelector('#guess').value;
 
     //check counter is < 10
     if (counter > 10) {
-        document.querySelector('answerIcon').className = "fas fa-times-circle";
-        document.querySelector('guessingZone').style.display = "none";
-        document.querySelector('loseZone').style.display = "block";
-        document.querySelector('losingNumber').innerHTML = secretNumber;
+        document.querySelector('#answerIcon').className = "fas fa-times-circle";
+        document.querySelector('#guessingZone').style.display = "none";
+        document.querySelector('#loseZone').style.display = "block";
+        document.querySelector('#losingNumber').innerHTML = secretNumber;
         document.body.style.backgroundColor = "red";
     } else {
 
         //check the guess number against the random number
         if (guessNumber > secretNumber) {
             //guess is more than secret number
-            document.querySelector('answerIcon').className = "fas fa-arrow-down";
+            document.querySelector('#answerIcon').className = "fas fa-arrow-down";
         } else if (guessNumber < secretNumber) {
             //guess is less than secret number
-            document.querySelector('answerIcon').className = "fas fa-arrow-up";
+            document.querySelector('#answerIcon').className = "fas fa-arrow-up";
         } else {
             // guess and secret number are equal
-            document.querySelector('answerIcon').className = "fas fa-check-circle";
-            document.querySelector('guessingZone').style.display = "none";
-            document.querySelector('winZone').style.display = "block";
-            document.querySelector('winningNumber').innerHTML = secretNumber;
+            document.querySelector('#answerIcon').className = "fas fa-check-circle";
+            document.querySelector('#guessingZone').style.display = "none";
+            document.querySelector('#winZone').style.display = "block";
+            document.querySelector('#winningNumber').innerHTML = secretNumber;
             document.body.style.backgroundColor = "limegreen";
         }
     }
     // Reset guess input
-    document.querySelector('guess').value = "";
+    document.querySelector('#guess').value = "";
 }
